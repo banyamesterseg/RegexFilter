931164fdab94042ac589c74c5e772a3e4def9de8
Harry McCormack <mccormack_harry@hotmail.com>
2021-08-08 09:57:21 +0100

    Initial commit

---
+++ src/main/java/me/hazedev/scf/SmartChatFilters.java
@@ -0,0 +1,12 @@
+package me.hazedev.scf;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SmartChatFilters extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
